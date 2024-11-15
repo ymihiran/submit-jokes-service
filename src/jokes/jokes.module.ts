e6c62f173bc9c6a16e4ed03e19c4a69b262cdc0b
Yasantha Mihiran <89309154+ymihiran@users.noreply.github.com>
2024-11-15 12:33:51 +0530

    jokes module update

--- src/jokes/jokes.module.ts
+++ src/jokes/jokes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { JokesController } from './jokes.controller';
 import { JokesService } from './jokes.service';
+import { JokesController } from './jokes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Joke, JokeSchema } from './schemas/joke.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Joke.name, schema: JokeSchema }])],
   controllers: [JokesController],
-  providers: [JokesService]
+  providers: [JokesService],
 })
 export class JokesModule {}
