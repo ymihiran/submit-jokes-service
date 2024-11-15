e38cf2138c50b1639a96a860348552fa4ada1483
Yasantha Mihiran <89309154+ymihiran@users.noreply.github.com>
2024-11-15 12:32:56 +0530

    app module implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JokesModule } from './jokes/jokes.module';
+import { ConfigModule } from '@nestjs/config';
+import { Joke, JokeSchema } from './jokes/schemas/joke.schema';
 
 @Module({
-  imports: [JokesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    JokesModule,
+    MongooseModule.forFeature([{ name: Joke.name, schema: JokeSchema }]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
