2e8b7aa6a4c84ba4edc51f8c125fa88a9ed2c0cd
Yasantha Mihiran <89309154+ymihiran@users.noreply.github.com>
2024-11-15 12:29:13 +0530

    contoller with basic operations added

--- src/jokes/jokes.controller.ts
+++ src/jokes/jokes.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+// src/jokes/jokes.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Joke, JokeDocument } from './schemas/joke.schema';
+import { Model } from 'mongoose';
+import { CreateJokeDto } from './dto/create-joke.dto';
 
-@Controller('jokes')
-export class JokesController {}
+@Injectable()
+
+export class JokesService {
+  constructor(@InjectModel(Joke.name) private jokeModel: Model<JokeDocument>) {}
+
+  async create(createJokeDto: CreateJokeDto): Promise<Joke> {
+    const createdJoke = new this.jokeModel(createJokeDto);
+    return createdJoke.save();
+  }
+
+  async findAll(): Promise<Joke[]> {
+    return this.jokeModel.find().exec();
+  }
+
+  async findByType(type: string): Promise<Joke[]> {
+    return this.jokeModel.find({ type }).exec();
+  }
+
+  async findPending(): Promise<Joke[]> {
+    // TODO: Implement logic
+    return this.jokeModel.find().exec();
+  }
+
+}
